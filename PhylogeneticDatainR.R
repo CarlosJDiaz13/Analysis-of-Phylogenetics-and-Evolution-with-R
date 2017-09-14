3209e8dfa27d7c7bf5aa6c188154107d8e79a09f
Carlos Diaz Acevedo <cjda1396@gmail.com>
2017-09-13 21:35:03 -0500

    Update PhylogeneticDatainR.R

--- PhylogeneticDatainR.R
+++ PhylogeneticDatainR.R
@@ -129,3 +129,86 @@ PrintName <- function(Codigos){
 # example with the funtion PrintName
 PrintName()
 
+# now other things
+#-------------------------------------------------------------------------------------------------------------
+#Print, wi length of each sequence.th a single command, the
+
+vec1 <- paste("U157",17:24,sep = "")
+MisDat <- read.GenBank(vec1)
+str(MisDat)
+str(MisDat[[1]])
+length(MisDat[[1]])
+
+
+#(c) Arrange the data in a matrix.
+
+x <- matrix(MisDat)
+x[,1]
+x[1,]
+
+as.alignment(MisDat) -> miPseudoAlin
+str(miPseudoAlin)
+class(miPseudoAlin)
+print(miPseudoAlin)
+str(miPseudoAlin[[2]])
+# write files in the format nexus
+write.nexus(miPseudoAlin,file = "pseudoalin",translate = T)
+write.nexus(MisDat,file = "misdat",translate = F)
+library(phangorn)
+#write files in the format fasta
+write.dna(MisDat,file = "misdat",format = "fasta")
+
+as.alignment(MisDat) -> miPseudoAlin
+str(miPseudoAlin)
+class(miPseudoAlin)
+print(miPseudoAlin)
+str(miPseudoAlin[[2]][1])
+#here convert of number to letters
+miPseudoCHar <- as.character(MisDat)
+str(miPseudoCHar)
+miPseudoCHar[[1]][3]
+str(miPseudoCHar[[1]])
+miPseudoCHar$U15719[3]
+miPseudoCHar[[1]][1:3]
+
+miPseudoCHar[1:8][z]
+
+
+# NOW as.matrix
+x2 <- as.matrix(miPseudoCHar)
+x2
+str(x2)
+cosito <- as.data.frame(x2)
+cosito
+str(cosito)
+cosito[,z]
+
+cosito[z,]
+##------------------------------------------------
+#(d) Extract and store in three matrices the first, the second, and the
+#third codon positions of all sequences. Compute their base frequencies.What do you conclude?
+
+z <- seq(1,1045,by = 3)
+seq(2,1045,by = 3)
+seq(3,1045,by = 3)
+
+cosito <- as.data.frame(x2)
+cosito
+class(cosito)
+str(cosito)
+cosito[,z]
+
+cosito[z,]
+cosito[1,9]
+cosito[1,8]
+#----------------------------------------------------------
+
+cosito <- as.data.frame(miPseudoCHar)
+cosito
+class(cosito)
+str(cosito)
+cosito[,z]
+
+cosito[z,]
+cosito[1,9]
+cosito[1,8]
